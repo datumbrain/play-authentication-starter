224f31078ba8e3b687434bef1c1a1b8835fa0901
saadali5997 <saadali5997@gmail.com>
2018-11-15 14:34:58 +0500

    Forms: WIP

--- app/controllers/CountController.scala
+++ app/controllers/CountController.scala
@@ -1,30 +1,47 @@
 package controllers
 
 import javax.inject._
-
 import play.api.mvc._
 import services.Counter
+import play.api.mvc._
+
+import scala.concurrent.Future
 
 /**
- * This controller demonstrates how to use dependency injection to
- * bind a component into a controller class. The class creates an
- * `Action` that shows an incrementing count to users. The [[Counter]]
- * object is injected by the Guice dependency injection system.
- */
+  * This controller demonstrates how to use dependency injection to
+  * bind a component into a controller class. The class creates an
+  * `Action` that shows an incrementing count to users. The [[Counter]]
+  * object is injected by the Guice dependency injection system.
+  */
 @Singleton
-class CountController @Inject() (cc: ControllerComponents,
-                                 counter: Counter) extends AbstractController(cc) {
+class CountController @Inject()(components: MessagesControllerComponents, cc: ControllerComponents, counter: Counter)
+  extends MessagesAbstractController(components) {
+
+  import models.forms.UserForm._
 
   /**
-   * Create an action that responds with the [[Counter]]'s current
-   * count. The result is plain text. This `Action` is mapped to
-   * `GET /count` requests by an entry in the `routes` config file.
-   */
+    * Create an action that responds with the [[Counter]]'s current
+    * count. The result is plain text. This `Action` is mapped to
+    * `GET /count` requests by an entry in the `routes` config file.
+    */
   def count = Action { implicit request =>
 
     Redirect(routes.HomeController.index()).withSession(
-    "userId" -> "useridrandom",
-    "roles" -> "MYROLE1"
-  ) }
+      "userId" -> "useridrandom",
+      "roles" -> "MYROLE1"
+    )
+  }
 
+  def signUpSave = Action { implicit request =>
+    Redirect(routes.HomeController.index()).withSession(
+      "userId" -> "useridrandom",
+      "roles" -> "MYROLE1"
+    )
+  }
+
+  private val postUrl = routes.CountController.signUpSave()
+
+  def signUp = Action { implicit request: MessagesRequest[AnyContent] =>
+    Ok(views.html.signUp(form, postUrl))
+  }
 }
--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -5,23 +5,23 @@ import javax.inject._
 import play.api.mvc._
 
 import scala.concurrent.Future
-
+import models.forms._
 import scala.concurrent.ExecutionContext.Implicits.global
 
 /**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
+  * This controller creates an `Action` to handle HTTP requests to the
+  * application's home page.
+  */
 @Singleton
-class HomeController @Inject()(cc: ControllerComponents, deadbolt: DeadboltActions) (implicit assetsFinder: AssetsFinder)
+class HomeController @Inject()(cc: ControllerComponents, deadbolt: DeadboltActions)(implicit assetsFinder: AssetsFinder)
   extends AbstractController(cc) {
 
   /**
-   * Create an Action to render an HTML page with a welcome message.
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
+    * Create an Action to render an HTML page with a welcome message.
+    * The configuration in the `routes` file means that this method
+    * will be called when the application receives a `GET` request with
+    * a path of `/`.
+    */
   def index: EssentialAction = deadbolt.SubjectPresent()() { implicit request =>
     Future(Ok(views.html.index("Your new application is ready.")))
   }
